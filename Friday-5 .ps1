a1f6fab0d84bd2509c29139c71fcf90b72478b5a
AspiringCoderNZ <deerstreamnz@gmail.com>
2023-09-01 12:17:44 +1200

    Task1

---
+++ Friday-5 .ps1
@@ -0,0 +1,9 @@
+﻿# installing Windows ADK 
+cd "C:\Users\Administrator\Desktop\New folder\Windows Kits\10\ADK" 
+Start-Process -FilePath adksetup.exe -ArgumentList "/S" -Wait
+#Install Windows PE
+cd "C:\Users\Administrator\Desktop\New folder\Windows Kits\10\ADKWinPEAddons" 
+Start-Process -FilePath adkwinpesetup.exe -ArgumentList "/S" -Wait
+
+
+
